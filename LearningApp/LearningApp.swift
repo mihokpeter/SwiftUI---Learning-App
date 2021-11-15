7c29877f111976a7dd0a9b2e82d0dd836858455a
Peter Mihok <peter.mihok115@gmail.com>
2021-11-15 20:46:21 +0100

    - rename contentView as well - organize files into folders - add content model - set content model as environment object

--- LearningApp/LearningApp.swift
+++ LearningApp/LearningApp.swift
@@ -1,5 +1,5 @@
 //
-//  LearningAppApp.swift
+//  LearningApp.swift
 //  LearningApp
 //
 //  Created by Peter Mihók on 15/11/2021.
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct LearningAppApp: App {
+struct LearningApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Peter Mihók on 15/11/2021.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
